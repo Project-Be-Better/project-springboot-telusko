69a23d13ce0c85aab2375029eddf7fdc780cfe73
sree <sreeraj_ec@hotmail.com>
2025-11-12 07:33:28 +0800

    20251112

---
+++ SimpleReactForm/backend/src/main/java/com/xploretown/backend/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.xploretown.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hello";
+    }
+
+}
