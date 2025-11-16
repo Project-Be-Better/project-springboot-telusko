e10ecf6ca54b4d077ca205e5004969460cd91e19
sree <sreeraj_ec@hotmail.com>
2025-11-17 05:28:31 +0800

    20251117

--- SimpleReactForm/frontend/src/App.tsx
+++ SimpleReactForm/frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import ProductList from "./components/ProductList";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const App = () => {
   return (
     <div>
-      <ProductList />
+      Navbar
+      <Routes>
+        <Route path="/" element={<Navigate to={"/products"} />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+      </Routes>
+      Footer
     </div>
   );
 };
--- SimpleReactForm/frontend/src/components/Product.tsx
+++ SimpleReactForm/frontend/src/components/Product.tsx
@@ -9,8 +9,15 @@ type ProductProps = {
 };
 
 const Product = ({ product }: ProductProps) => {
+  const handleClick = () => {
+    console.log("Clicked on the component ", product.name);
+  };
+
   return (
-    <div className="flex gap-2 rounded-2xl bg-white px-2 py-4 shadow-md">
+    <div
+      className="flex gap-2 rounded-2xl bg-white px-2 py-4 shadow-md"
+      onClick={handleClick}
+    >
       <p>{product.id}</p>
       <h2 className="font-bold">{product.name}</h2>
       <p>{product.price}</p>
